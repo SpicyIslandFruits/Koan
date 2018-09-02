e650ee65cb506c80ab2e3add3ed8364ea9f2f11f
SpicyIslandFruits <SpicyIsland1998@gmail.com>
2018-09-02 13:45:36 +0900

    webViewの実装

--- app/src/main/java/com/example/spicyisland/koan/WebViewActivity.kt
+++ app/src/main/java/com/example/spicyisland/koan/WebViewActivity.kt
@@ -1,9 +1,8 @@
 package com.example.spicyisland.koan
 
-import android.graphics.Bitmap
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Message
 import android.view.KeyEvent
 import android.webkit.*
 import android.widget.Toast
@@ -11,17 +10,20 @@ import kotlinx.android.synthetic.main.activity_web_view.*
 
 class WebViewActivity : AppCompatActivity() {
 
+    var isFirstRequest = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_web_view)
         val webView = webView
         webView.webChromeClient = WebChromeClient()
-        webView.webViewClient = WebViewClient()
+        webView.webViewClient = MyWebViewClient()
         webView.clearCache(true)
         webView.clearHistory()
         webView.settings.javaScriptEnabled = true
         webView.settings.javaScriptCanOpenWindowsAutomatically = true
         webView.loadUrl(KoanMainPage)
+
     }
 
     override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
@@ -31,6 +33,15 @@ class WebViewActivity : AppCompatActivity() {
         }
 
         return super.onKeyDown(keyCode, event)
+    }
+
+    inner class MyWebViewClient : WebViewClient(){
+        override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+            if (Build.VERSION.SDK_INT >= 24 && request!!.isRedirect && isFirstRequest)
+                Toast.makeText(this@WebViewActivity, R.string.still_getting_cookie, Toast.LENGTH_LONG).show()
+            isFirstRequest = false
+            return super.shouldOverrideUrlLoading(view, request)
+        }
     }
 
 }
