135d5394d05e376920db687c08c899032cb7d298
SpicyIslandFruits <SpicyIsland1998@gmail.com>
2018-09-02 13:21:51 +0900

    webViewの実装

--- app/src/main/java/com/example/spicyisland/koan/WebViewActivity.kt
+++ app/src/main/java/com/example/spicyisland/koan/WebViewActivity.kt
@@ -1,11 +1,11 @@
 package com.example.spicyisland.koan
 
+import android.graphics.Bitmap
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Message
 import android.view.KeyEvent
-import android.webkit.CookieManager
-import android.webkit.WebChromeClient
-import android.webkit.WebViewClient
+import android.webkit.*
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_web_view.*
 
@@ -14,11 +14,6 @@ class WebViewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_web_view)
-        //Cookieの数が2でも３でもないときはnullを返し、トーストで自動ログインできないことを伝える
-        if(KoanService.getCookieMapFromCookieManager() == null
-                && KoanService.getCookieMapFromCookieManager(KoanUrl, 2) == null){
-            Toast.makeText(this, getText(R.string.still_getting_cookie), Toast.LENGTH_LONG).show()
-        }
         val webView = webView
         webView.webChromeClient = WebChromeClient()
         webView.webViewClient = WebViewClient()
@@ -37,14 +32,5 @@ class WebViewActivity : AppCompatActivity() {
 
         return super.onKeyDown(keyCode, event)
     }
-//気が向いたらブラウザのロードの表示をします。
-//    inner class MyWebViewClient : WebViewClient() {
-//        override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
-//            super.onPageStarted(view, url, favicon)
-//        }
-//
-//        override fun onPageFinished(view: WebView?, url: String?) {
-//            super.onPageFinished(view, url)
-//        }
-//    }
+
 }
