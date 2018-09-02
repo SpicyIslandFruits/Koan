b4635399ecb4c1668389a2e477e8c11c85e3d3e3
SpicyIslandFruits <SpicyIsland1998@gmail.com>
2018-09-03 02:55:15 +0900

    超微調整

--- app/src/main/java/com/example/spicyisland/koan/MainActivity.kt
+++ app/src/main/java/com/example/spicyisland/koan/MainActivity.kt
@@ -1,40 +1,47 @@
 package com.example.spicyisland.koan
 
-import android.os.Bundle
-import android.support.design.widget.BottomNavigationView
+import android.support.design.widget.TabLayout
 import android.support.v7.app.AppCompatActivity
+
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+import android.os.Bundle
+
 import kotlinx.android.synthetic.main.activity_main.*
 
-/**
- * TODO: 時間割を初めて取得しに行く際に、連打されると固まるのでどうにかする
- * TODO: 時間割取得中はプログレスバーを回す
- * TODO: しばらくするとクッキーが無効になるので、クッキーが無効になった際の処理も書く、バックグラウンドで取得し直すのがいいかもしれない
- */
 class MainActivity : AppCompatActivity() {
 
-    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.navigation_curriculum -> {
-                supportFragmentManager.beginTransaction().replace(R.id.fragment, CurriculumFragment()).commit()
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_notifications -> {
-                supportFragmentManager.beginTransaction().replace(R.id.fragment, NotificationFragment()).commit()
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_account -> {
-                supportFragmentManager.beginTransaction().replace(R.id.fragment, AccountFragment()).commit()
-                return@OnNavigationItemSelectedListener true
-            }
-        }
-        false
-    }
+    private var mSectionsPagerAdapter: SectionsPagerAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        supportActionBar!!.elevation = 8f
-        supportFragmentManager.beginTransaction().replace(R.id.fragment, CurriculumFragment()).commit()
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+
+        setSupportActionBar(toolbar)
+        mSectionsPagerAdapter = SectionsPagerAdapter(supportFragmentManager)
+
+        container.adapter = mSectionsPagerAdapter
+
+        container.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabs))
+        tabs.addOnTabSelectedListener(TabLayout.ViewPagerOnTabSelectedListener(container))
+
     }
+
+    inner class SectionsPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+
+        override fun getItem(position: Int): Fragment {
+            when(position){
+                0 -> return CurriculumFragment()
+                1 -> return NotificationFragment()
+                2 -> return AccountFragment()
+            }
+            return CurriculumFragment()
+        }
+
+        override fun getCount(): Int {
+            return 3
+        }
+    }
+
 }
