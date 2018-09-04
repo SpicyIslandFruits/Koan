dd7b83280799872a3bd916bb3b7ccb39bad337f4
SpicyIslandFruits <SpicyIsland1998@gmail.com>
2018-09-04 11:32:56 +0900

    通信部分を一新するため、今までの復讐としてコメントをつけていっている

--- app/src/main/java/com/example/spicyisland/koan/AccountFragment.kt
+++ app/src/main/java/com/example/spicyisland/koan/AccountFragment.kt
@@ -1,6 +1,5 @@
 package com.example.spicyisland.koan
 
-import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
@@ -12,21 +11,35 @@ import io.realm.Realm
 import kotlinx.android.synthetic.main.fragment_account.*
 import android.support.v7.app.AlertDialog
 
+/**
+ * アカウント画面
+ * TODO: フィードバックボタンとよくある質問画面の作成(MainActivityの子アクティビティ)
+ */
 class AccountFragment : Fragment() {
 
+    /**
+     * レイアウトのインフレート
+     */
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_account, container, false)
     }
 
+    /**
+     * ボタンのonClickListenerの設定
+     */
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         logOutButton.setOnClickListener {
-            logOutButton.isEnabled = false
             createLogoutDialog()
         }
     }
 
+    /**
+     * クッキーとrealmのユーザーオブジェクトを削除して
+     * TODO: StartActivityを削除してLoginActivityにする
+     * ログインアクティビティを起動し、メインアクティビティを終了
+     */
     private fun logout(){
         removeAllCookies()
         removeAllRealmUserObject()
@@ -34,6 +47,11 @@ class AccountFragment : Fragment() {
         activity!!.finish()
     }
 
+    /**
+     * realmからユーザークラスのデータをすべて削除するメソッド
+     * TODO: 使いまわせるように、一つのクラスにまとめる
+     * TODO: 非同期で使うかもしれないので変数はメソッドの中で宣言する
+     */
     private fun removeAllRealmUserObject(){
         val realm = Realm.getDefaultInstance()
         val oldUser = realm.where(User::class.java).findAll()
@@ -43,12 +61,24 @@ class AccountFragment : Fragment() {
         realm.close()
     }
 
+    /**
+     * このアプリによるクッキーをすべて削除するメソッド
+     * TODO: 使いまわせるように一つのクラスにすべてまとめる
+     * TODO: 非同期で使うかもしれないので変数はメソッドの中で宣言する
+     */
     private fun removeAllCookies() {
         val cookieManager = CookieManager.getInstance()
         cookieManager.removeAllCookies(null)
     }
 
+    /**
+     * ダイヤログを作成するメソッド
+     * 連打防止のためにボタンを無効化してからダイヤログを作成し、ダイヤログがなくなるときにボタンを有効化
+     */
     private fun createLogoutDialog(){
+
+        logOutButton.isEnabled = false
+
         AlertDialog.Builder(this.context!!)
                 .setTitle(R.string.logout_conformation_title)
                 .setMessage(R.string.logout_confirmation_message)
