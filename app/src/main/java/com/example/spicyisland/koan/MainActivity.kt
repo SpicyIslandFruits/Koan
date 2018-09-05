92791897eb7dde920d49b18bef01ef7e44af7b21
SpicyIslandFruits <SpicyIsland1998@gmail.com>
2018-09-05 10:36:52 +0900

    リポジトリ名を変えた

--- app/src/main/java/com/example/spicyisland/koan/MainActivity.kt
+++ app/src/main/java/com/example/spicyisland/koan/MainActivity.kt
@@ -27,7 +27,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        
+
         setContentView(R.layout.activity_main)
         supportActionBar!!.elevation = 8f
         supportFragmentManager.beginTransaction().replace(R.id.fragment, CurriculumFragment()).commit()
