1052bbe3063ee5178a64ccafb7249b187b107622
SpicyIslandFruits <SpicyIsland1998@gmail.com>
2018-09-05 21:17:45 +0900

    微修正

--- app/src/main/java/com/example/spicyisland/koan/TagPositions.kt
+++ app/src/main/java/com/example/spicyisland/koan/TagPositions.kt
@@ -6,3 +6,10 @@ package com.example.spicyisland.koan
  * 36個ある
  */
 val curriculumTagPositions: ArrayList<Int> = arrayListOf(14, 15, 16, 17, 18, 19, 21, 22, 23, 24, 25, 26, 28, 29, 30, 31, 32, 33, 35, 36, 37, 38, 39, 40, 42, 43, 44, 45, 46, 47, 49, 50, 51, 52, 53, 54)
+
+/**
+ * 各々の掲示のページの、掲示内容が書かれたタグの場所
+ * 気が向いたら数えてアップデートなどで表示できるようにする
+ * 今はとりあえず掲示板押したらブラウザで開く
+ */
+val bulletinTagPositions: ArrayList<Int> = arrayListOf()
