8049f51327dd8206958c4b3d7ff1370c9fa0eb9e
SpicyIslandFruits <SpicyIsland1998@gmail.com>
2018-09-05 11:00:39 +0900

    test

--- app/src/main/java/com/example/spicyisland/koan/KoanService.kt
+++ app/src/main/java/com/example/spicyisland/koan/KoanService.kt
@@ -100,4 +100,28 @@ object KoanService {
                     .cookies(koanCookies).followRedirects(false).method(Connection.Method.POST).execute().cookies())
         }
     }
+
+    /**
+     * クッキーストアからクッキーを取得するメソッド
+     * クッキーの数はkoanでは3のはずなので、デフォルトではそれ以外の個数が帰ってきたらnullを返す
+     * 予想されるクッキーの数はオプションで変更可能
+     */
+    fun getCookieMapFromCookieManager(url: String = KoanUrl, expectedCookieSize: Int = 3): MutableMap<String, String>? {
+        val cookieManager = CookieManager.getInstance()
+        val koanCookiesString = cookieManager.getCookie(url)
+        var koanCookies: MutableMap<String, String>? = mutableMapOf()
+        if (koanCookiesString != null) {
+            val koanCookie = koanCookiesString.split("=", ";")
+            for (i in 0 until koanCookie.size step 2)
+                koanCookies!![koanCookie[i]] = koanCookie[i + 1]
+
+            if (koanCookies!!.size != expectedCookieSize){
+                koanCookies = null
+            }
+        }else{
+            koanCookies = null
+        }
+
+        return koanCookies
+    }
 }
